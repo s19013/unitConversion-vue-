68998b8c49136fb8dc427ba2c61fd4364c4cd9bc
知念英弥 <s19013@std.it-college.ac.jp>
2022-05-29 22:42:25 +0900

    数秒でできた

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            
+            km:0,
+            m:0.
+        }
+    },
+    computed:{
+
+    },
+    watch:{
+        km:function (value) {
+            this.km = value
+            this.m  = value * 1000
+        },
+        m:function (value){
+            this.km = value/1000
+            this.m  = value
         }
     },
     methods: {
